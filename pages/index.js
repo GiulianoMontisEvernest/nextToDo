e62ff25d0385a129a861449a37bec74414147517
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-01-11 12:40:30 +0100

    mui test positive

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+// mui
+import { Typography } from '@mui/material';
+
+// mui icons
 
 export default function Home() {
   return (
@@ -12,9 +17,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Typography variant="h1" className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </Typography>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -65,5 +70,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
